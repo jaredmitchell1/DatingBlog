9426726145c7ca09b85669b39ec858a79317accc
Unknown <jaredmitchell@outlook.com>
2017-01-01 22:34:53 -0800

    Creating the basic app

---
+++ src/Scripts/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import '../Resources/App.css';
+import Header from './Header';
+import Post from './Post';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
